3f88b14022dffbf44a88a1a924a85c359539d096
takuyamashita <taku.pro-myfam@ezweb.ne.jp>
2021-03-30 02:22:44 +0900

    update hostname

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const buldClient = ({ req }) => {
     if(typeof window === 'undefined'){
         return axios.create({
-            baseURL: 'https://157.230.201.202',
+            baseURL: 'http://157.230.201.202/',
             headers: req.headers
         });
     }else{
